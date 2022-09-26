aa288b0c547b068d79b8521de50e4b5a783de6bd
bakatarekun <ckyamanaka@gmail.com>
2022-09-26 10:27:45 -0400

    Add auto fill in index page

--- CS540Project2/myapp/views.py
+++ CS540Project2/myapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here. 123457890
 def login(request):
     return render(request, "myapp/login.html",{})
 @login_required
 def index(request):
-    return render(request, "myapp/index.html",{})
+    data = User.objects.raw("SELECT * FROM auth_user;")
+    return render(request, "myapp/index.html",{'data' : data})
 @login_required
 def home(request):
     return render(request, "myapp/home.html",{})
