ab7aaff50d9a2216e94636be276a9de8c3b18f80
bakatarekun <ckyamanaka@gmail.com>
2022-09-26 08:40:59 -0400

    logout link add

--- CS540Project2/CS540Project2/urls.py
+++ CS540Project2/CS540Project2/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls import url, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  include('myapp.urls')),
-
+    path("accounts/", include("django.contrib.auth.urls")), 
    
 ]
 
